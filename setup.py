afca7f2334bd809e6c7bc17a12433754c9114429
RafaelTakata0105 <rafaeltakata0105@gmail.com>
2024-03-04 19:58:13 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The final project for the class of programming for data analytics',
+    author='Rafael Takata',
+    license='MIT',
+)
